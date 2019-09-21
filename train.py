7e4485f5ad50e8db858b4a1a55f82cfae48580ab
ekazakos <ek17557@bristol.ac.uk>
2019-09-21 15:55:17 +0100

    try sgd

--- train.py
+++ train.py
@@ -71,9 +71,14 @@ def main():
 
     criterion = torch.nn.CrossEntropyLoss()
 
-    optimizer = torch.optim.Adam(model.parameters(),
-                                 lr=args.lr,
-                                 weight_decay=args.weight_decay)
+#    optimizer = torch.optim.Adam(model.parameters(),
+#                                 lr=args.lr,
+ #                                weight_decay=args.weight_decay)
+
+    optimizer = torch.optim.SGD(model.parameters(),
+                                lr=args.lr,
+                                momentum=args.momentum,
+                                weight_decay=args.weight_decay)
 
     for epoch in range(args.epochs):
         train(train_loader, model, criterion, optimizer, epoch, device)
