4e9f80e33642b270e972420c70728b31a667c72e
ekazakos <ek17557@bristol.ac.uk>
2019-09-21 17:11:31 +0100

    back to maxpool after conv4

--- models.py
+++ models.py
@@ -5,28 +5,28 @@ from torch import nn
 class ESCModel(nn.Module):
     def __init__(self):
         super(ESCModel, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, stride=2, padding=1)
+        self.conv1 = nn.Conv2d(1, 32, 3, stride=1, padding=1)
         self.batch_norm1 = nn.BatchNorm2d(32)
         self.relu1 = nn.ReLU()
 
-        self.conv2 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(32, 32, 3, stride=1, padding=1)
         self.batch_norm2 = nn.BatchNorm2d(32)
         self.relu2 = nn.ReLU()
         self.max_pool2 = nn.MaxPool2d(2)
         self.dropout2 = nn.Dropout2d(p=0.5)
 
-        self.conv3 = nn.Conv2d(32, 64, 3, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 3, stride=1, padding=1)
         self.batch_norm3 = nn.BatchNorm2d(64)
         self.relu3 = nn.ReLU()
 
-        self.conv4 = nn.Conv2d(64, 64, 3, stride=2, padding=1)
+        self.conv4 = nn.Conv2d(64, 64, 3, stride=1, padding=1)
         self.batch_norm4 = nn.BatchNorm2d(64)
         self.relu4 = nn.ReLU()
-        #self.max_pool4 = nn.MaxPool2d(2)
+        self.max_pool4 = nn.MaxPool2d(2)
         self.dropout4 = nn.Dropout2d(p=0.5)
 
 
-        self.fc1 = nn.Linear(64*3*2, 1024)
+        self.fc1 = nn.Linear(64*10*21, 1024)
         self.sigm = nn.Sigmoid()
         self.dropout_fc1 = nn.Dropout(p=0.5)
         self.fc2 = nn.Linear(1024, 10)
@@ -49,7 +49,7 @@ class ESCModel(nn.Module):
         x = self.conv4(x)
         x = self.batch_norm4(x)
         x = self.relu4(x)
-        #x = self.max_pool4(x)
+        x = self.max_pool4(x)
         x = self.dropout4(x)
 
         x = x.flatten(1)
