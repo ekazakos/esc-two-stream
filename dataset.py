8ab4c33408f6a48ba853c13688ac9e95efeb76e4
ekazakos <ek17557@bristol.ac.uk>
2019-09-21 14:26:08 +0100

    added forgoten dict key when loading features

--- dataset.py
+++ dataset.py
@@ -11,34 +11,34 @@ class UrbanSound8KDataset(data.Dataset):
 
     def __getitem__(self, index):
         if self.mode == 'LMC':
-            feature = np.vstack((self.dataset[index]['logmelspec'],
-                                 self.dataset[index]['chroma'],
-                                 self.dataset[index]['tonnetz'],
-                                 self.dataset[index]['spectral_contrast']))
+            feature = np.vstack((self.dataset[index]['features']['logmelspec'],
+                                 self.dataset[index]['features']['chroma'],
+                                 self.dataset[index]['features']['tonnetz'],
+                                 self.dataset[index]['features']['spectral_contrast']))
             feature = torch.from_numpy(feature)
         elif self.mode == 'MC':
-            feature = np.vstack((self.dataset[index]['mfcc'],
-                                 self.dataset[index]['chroma'],
-                                 self.dataset[index]['tonnetz'],
-                                 self.dataset[index]['spectral_contrast']))
+            feature = np.vstack((self.dataset[index]['features']['mfcc'],
+                                 self.dataset[index]['features']['chroma'],
+                                 self.dataset[index]['features']['tonnetz'],
+                                 self.dataset[index]['features']['spectral_contrast']))
             feature = torch.from_numpy(feature)
         elif self.mode == 'MLMC':
-            feature = np.vstack((self.dataset[index]['logmelspec'],
-                                 self.dataset[index]['mfcc'],
-                                 self.dataset[index]['chroma'],
-                                 self.dataset[index]['tonnetz'],
-                                 self.dataset[index]['spectral_contrast']))
+            feature = np.vstack((self.dataset[index]['features']['logmelspec'],
+                                 self.dataset[index]['features']['mfcc'],
+                                 self.dataset[index]['features']['chroma'],
+                                 self.dataset[index]['features']['tonnetz'],
+                                 self.dataset[index]['features']['spectral_contrast']))
             feature = torch.from_numpy(feature)
         elif self.mode == 'LMC+MC':
-            lmc = np.vstack((self.dataset[index]['logmelspec'],
-                             self.dataset[index]['chroma'],
-                             self.dataset[index]['tonnetz'],
-                             self.dataset[index]['spectral_contrast']))
+            lmc = np.vstack((self.dataset[index]['features']['logmelspec'],
+                             self.dataset[index]['features']['chroma'],
+                             self.dataset[index]['features']['tonnetz'],
+                             self.dataset[index]['features']['spectral_contrast']))
 
-            mc = np.vstack((self.dataset[index]['mfcc'],
-                            self.dataset[index]['chroma'],
-                            self.dataset[index]['tonnetz'],
-                            self.dataset[index]['spectral_contrast']))
+            mc = np.vstack((self.dataset[index]['features']['mfcc'],
+                            self.dataset[index]['features']['chroma'],
+                            self.dataset[index]['features']['tonnetz'],
+                            self.dataset[index]['features']['spectral_contrast']))
             feature = (torch.from_numpy(lmc), torch.from_numpy(mc))
         label = self.dataset[index]['classID']
         return feature, label
