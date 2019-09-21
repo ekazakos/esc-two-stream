a6b1d98412fc5095cba296bbaa17f62bb6b4f2fc
ekazakos <ek17557@bristol.ac.uk>
2019-09-21 14:59:33 +0100

    removed clip_gradient

--- train.py
+++ train.py
@@ -129,11 +129,6 @@ def train(train_loader, model, criterion, optimizer, epoch, device):
 
         loss.backward()
 
-        if args.clip_gradient is not None:
-            total_norm = clip_grad_norm_(model.parameters(), args.clip_gradient)
-            if total_norm > args.clip_gradient:
-                print("clipping gradient: {} with coef {}".format(total_norm, args.clip_gradient / total_norm))
-
         optimizer.step()
 
         training_iterations += 1
