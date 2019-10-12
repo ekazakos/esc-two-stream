b01205c7cbcfa6d54d69c58416940f28713f0ebc
ekazakos <ek17557@bristol.ac.uk>
2019-10-12 14:34:31 +0100

    re-add batch norm

--- models.py
+++ models.py
@@ -6,21 +6,21 @@ class ESCModel(nn.Module):
     def __init__(self):
         super(ESCModel, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, 3, stride=1, padding=1)
-        # self.batch_norm1 = nn.BatchNorm2d(32)
+        self.batch_norm1 = nn.BatchNorm2d(32)
         self.relu1 = nn.ReLU()
 
         self.conv2 = nn.Conv2d(32, 32, 3, stride=1, padding=1)
-        # self.batch_norm2 = nn.BatchNorm2d(32)
+        self.batch_norm2 = nn.BatchNorm2d(32)
         self.relu2 = nn.ReLU()
         self.max_pool2 = nn.MaxPool2d(2)
         self.dropout2 = nn.Dropout(p=0.5)
 
         self.conv3 = nn.Conv2d(32, 64, 3, stride=1, padding=1)
-        # self.batch_norm3 = nn.BatchNorm2d(64)
+        self.batch_norm3 = nn.BatchNorm2d(64)
         self.relu3 = nn.ReLU()
 
         self.conv4 = nn.Conv2d(64, 64, 3, stride=1, padding=1)
-        # self.batch_norm4 = nn.BatchNorm2d(64)
+        self.batch_norm4 = nn.BatchNorm2d(64)
         self.relu4 = nn.ReLU()
         self.max_pool4 = nn.MaxPool2d(2)
         self.dropout4 = nn.Dropout(p=0.5)
@@ -33,21 +33,21 @@ class ESCModel(nn.Module):
 
     def forward(self, input):
         x = self.conv1(input)
-        # x = self.batch_norm1(x)
+        x = self.batch_norm1(x)
         x = self.relu1(x)
 
         x = self.conv2(x)
-        # x = self.batch_norm2(x)
+        x = self.batch_norm2(x)
         x = self.relu2(x)
         x = self.max_pool2(x)
         x = self.dropout2(x)
 
         x = self.conv3(x)
-        # x = self.batch_norm3(x)
+        x = self.batch_norm3(x)
         x = self.relu3(x)
 
         x = self.conv4(x)
-        # x = self.batch_norm4(x)
+        x = self.batch_norm4(x)
         x = self.relu4(x)
         x = self.max_pool4(x)
         x = self.dropout4(x)
